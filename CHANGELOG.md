98e0c2958f8198963057f7dba68617024e8cd410
harlem88 <harlem88.ant@gmail.com>
2021-06-07 17:13:16 +0200

    AstarteDeviceIdUtils: add Astarte Device Id generation utility
    The utility helps generate a valid Astarte Device Id.
    Signed-off-by: harlem88 <harlem88.ant@gmail.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [1.0.0] - Unreleased
+### Added
+- Add methods that generate an Astarte Device ID using either a random UUIDv4 or by deriving an UUIDv5 using unique data.
+
 ## [1.0.0-rc.0] - 2021-05-10
 ### Added
 - [generic] Support to Maven Repository publish
---
+++ DeviceSDK/src/test/java/org/astarteplatform/devicesdk/AstarteDeviceIdUtilsTest.java
@@ -0,0 +1,46 @@
+package org.astarteplatform.devicesdk;
+
+import static org.junit.Assert.*;
+
+import java.util.Base64;
+import java.util.UUID;
+import org.junit.Test;
+
+public class AstarteDeviceIdUtilsTest {
+
+  @Test
+  public void testGenerateIdFromUUIDDefault() {
+    UUID nameUUID = UUID.fromString("f79ad91f-c638-4889-ae74-9d001a3b4cf8");
+    String hardwareId = "myidentifierdata";
+
+    String expectedDeviceId = "AJInS0w3VpWpuOqkXhgZdA";
+    String deviceId = AstarteDeviceIdUtils.generateId(nameUUID, hardwareId);
+    assertEquals(
+        "Compare with result from astartectl compute-from-string", expectedDeviceId, deviceId);
+  }
+
+  @Test
+  public void testGenerateIdFromUUID() {
+    UUID nameUUID = UUID.fromString("b068931c-c450-342b-a3f5-b3d276ea4297");
+    String hardwareId = "0099112233";
+
+    String expectedDeviceId = "dvt9mLDaWb2vW7bdBJwKCg";
+    String deviceId = AstarteDeviceIdUtils.generateId(nameUUID, hardwareId);
+    assertEquals(
+        "Compare with result from astartectl compute-from-string", expectedDeviceId, deviceId);
+  }
+
+  @Test
+  public void testGenerateUUID() {
+    String astarteDeviceId = AstarteDeviceIdUtils.generateId();
+    byte[] astarteDecodedId;
+    try {
+      astarteDecodedId = Base64.getUrlDecoder().decode(astarteDeviceId);
+    } catch (IllegalArgumentException e) {
+      astarteDecodedId = null;
+    }
+
+    assertNotNull("Id decoded != NULL", astarteDecodedId);
+    assertTrue("Id decoded not empty", astarteDecodedId.length > 0);
+  }
+}
